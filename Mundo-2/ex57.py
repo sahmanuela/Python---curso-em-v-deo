16fd64e5bae826b19baa892e2e6a37c4decd3783
sahmanuela <samantha.manuela@hotmail.com>
2022-11-03 16:56:45 -0300

    Exercícios python - curso em vídeo - MUNDO 2

--- Mundo-2/ex57.py
+++ Mundo-2/ex57.py
@@ -1,5 +1,9 @@
 # Exercício Python 57: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’. Caso esteja errado, peça a digitação novamente até ter um valor correto.
-sexo = input('Insira o sexo da pessoa - F para feminino e M para masculino: ').lower()
+print('-=' * 20)
+print('MASCULINO ou FEMININO')
+print('-=' * 20)
+
+sexo = input('Insira o sexo da pessoa - F/M: ').lower()
 while sexo != "f" and sexo != "m":
     print('Opção inválida, tente novamente!')
     sexo = input('Insira o sexo da pessoa - F para feminino e M para masculino: ').lower()

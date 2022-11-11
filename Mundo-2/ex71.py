4dd5b0e9ad653f412da7dc862df9ea4c1605408a
sahmanuela <samantha.manuela@hotmail.com>
2022-11-11 05:26:30 -0300

    Exercícios python - curso em vídeo - MUNDO 2

--- Mundo-2/ex71.py
+++ Mundo-2/ex71.py
@@ -2,6 +2,28 @@
 
 # considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
 print('-=' * 20)
-print('CAIXA ELETRÔNICO!')
+print('{:^40}'.format('CAIXA ELETRÔNICO!\n'))
 print('Cédulas disponíveis: R$50, R$20, R$10 e R$1.')
 print('-=' * 20)
+
+saque = int(input('Insira o valor a ser sacado: R$'))
+total = saque
+cedula = 50 #começando pelo maior valor disponível
+totalCedulas = 0
+
+while True:
+    if total >= cedula:
+        total -= cedula
+        totalCedulas += 1
+    else:
+        if totalCedulas > 0:
+            print(f'Total de {totalCedulas} de R${cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totalCedulas = 0
+        if total == 0:
+            break

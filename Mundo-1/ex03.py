b04667c94b42d2bf8e2403be34de6e41aaca444f
Samantha Ferri <samantha.manuela@hotmail.com>
2023-03-30 15:42:19 -0300

    exercicio 03

--- Mundo-1/ex03.py
+++ Mundo-1/ex03.py
@@ -4,4 +4,4 @@ n1 = float(input('Insira o primeiro número: '))
 n2 = float(input('Insira o segundo número: '))
 soma = n1 + n2
 
-print(f'A soma entre {n1} e {n2} é {soma:.2f}')
+print(f'{n1} + {n2} = {soma:.2f}')

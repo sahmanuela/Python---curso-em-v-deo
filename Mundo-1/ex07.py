091dd3667f92e75c4dfca0b71bb1a0c5118ae783
Samantha Ferri <samantha.manuela@hotmail.com>
2023-03-30 15:46:47 -0300

    Exercício 07

--- Mundo-1/ex07.py
+++ Mundo-1/ex07.py
@@ -1,5 +1,5 @@
 # Exercício Python 7: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
-n1 = float(input('Insira o primeiro numero: '))
-n2 = float(input('Insira o segundo numero: '))
+n1 = float(input('Insira a primeira nota: '))
+n2 = float(input('Insira a segunda nota: '))
 media = (n1 + n2)/2
-print(f'Média: {media:.2f}')
+print(f'Média do aluno: {media:.2f}')

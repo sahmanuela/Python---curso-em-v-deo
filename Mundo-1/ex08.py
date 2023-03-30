6ab52228376ac62f358b376af590ec3c45dc7fec
Samantha Ferri <samantha.manuela@hotmail.com>
2023-03-30 15:48:22 -0300

    Exercício 08

--- Mundo-1/ex08.py
+++ Mundo-1/ex08.py
@@ -3,4 +3,4 @@ medida = float(input('Insira um valor em metros: '))
 cm = medida * 100
 mm = medida * 1000
 
-print(f'Número: {medida}\ncm: {cm}\nmm: {mm}')
+print(f'Número: {medida} metros\nCentímetro: {cm} cm\nMilímetro: {mm} mm')
